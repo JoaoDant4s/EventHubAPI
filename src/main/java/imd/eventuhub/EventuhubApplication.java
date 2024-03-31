c72c4e5c06da9b3124cca5c7c18d1341c09e3887
João Batista Serino Dantas <joao.dantas@venturus.org.br>
2024-03-31 18:13:04 -0300

    feat: add project's ER diagram

---
+++ src/main/java/imd/eventuhub/EventuhubApplication.java
@@ -0,0 +1,13 @@
+package imd.eventuhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuhubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/imd/eventuhub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package imd.eventuhub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventuhubApplication.class);
+	}
+
+}
